8aead7720de6bdab06a2ebda762e67d5b4ce2259
Oleksii Shtanko <shtankopro@gmail.com>
2022-04-19 01:02:49 +0300

    Add tests

--- test/repository/email_list_repository_test.dart
+++ test/repository/email_list_repository_test.dart
@@ -29,6 +29,29 @@ void main() {
           completion(equals([])),
         );
       });
+
+      test('returns correct lists', () async {
+        final list = EmailListRepository().loadData();
+        await list.then((values) {
+          expect(values, hasLength(6));
+          expect(
+            values.contains(
+              Email(
+                sender: 'Ralph Edwards',
+                subject: 'The results to our user testing',
+                messagePreview: 'What is the progress on that task?',
+                isFavorite: false,
+                date: DateTime.parse('2022-04-10 20:18:04Z'),
+                image:
+                    'https://external-preview.redd.it/STUmd55tKPrA-gq3y2wqicFTkWL-7oUij0SazqkGylE.jpg?auto=webp&s=6b1cd0e4976a852c9a805cf54a7bb7a19d169063',
+                attachments: [],
+              ),
+            ),
+            true,
+          );
+        });
+        expect(list, completes);
+      });
     });
   });
 }
