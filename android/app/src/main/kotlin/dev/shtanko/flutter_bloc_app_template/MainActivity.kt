8a8f8bb29c8583c95ec6f54c5811fcdbeb36980c
Oleksii Shtanko <shtankopro@gmail.com>
2022-04-22 18:33:54 +0300

    Add release properties for android + small refactor

--- android/app/src/main/kotlin/dev/shtanko/flutter_bloc_app_template/MainActivity.kt
+++ android/app/src/main/kotlin/dev/shtanko/flutter_bloc_app_template/MainActivity.kt
@@ -1,6 +1,11 @@
 package dev.shtanko.flutter_bloc_app_template
 
+import android.os.Bundle
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity: FlutterActivity() {
+class MainActivity : FlutterActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        // TODO
+    }
 }
--- lib/widgets/separator.dart
+++ lib/widgets/separator.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:gap/gap.dart';
 
-/// Default space values that can be used on a [Separator] generated object.
 class Space {
   static const superLarge = 24.0;
   static const large = 16.0;
@@ -13,17 +12,11 @@ class Space {
   static const superSmall = 4.0;
 }
 
-class Height {}
-
-/// This class is a collection of various already-defined
-/// separators, such as spacers and dividers.
 class Separator {
-  /// Default gap spacer. Defaults to [Space.medium].
   static Widget spacer([double space = Space.medium]) {
     return Gap(space);
   }
 
-  /// Adds a [Gap] widget between all children inside the [children] list.
   static List<Widget> spaceChildren({
     double space = Space.medium,
     required List<Widget> children,
@@ -31,7 +24,6 @@ class Separator {
     return children.separate(space);
   }
 
-  /// Divider with height 0, as thin as a piece of paper.
   static Widget divider({
     double indent = 0,
     double? endIndent,
@@ -40,14 +32,12 @@ class Separator {
     return Divider(height: 0, indent: indent);
   }
 
-  /// Empty widget, there's nothing more to say.
   static Widget none() {
     return const Gap(0);
   }
 }
 
 extension ListGutter on List<Widget> {
-  /// Adds a [Gap] widget between all children inside the list.
   List<Widget> separate([double space = Space.medium]) => length <= 1
       ? this
       : sublist(1).fold(
