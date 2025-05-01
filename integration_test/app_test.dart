c77f38b70fb48b9de2ace451afc08d814146b489
Oleksii Shtanko <shtankopro@gmail.com>
2025-05-01 23:24:36 +0100

    WIP

--- integration_test/app_test.dart
+++ integration_test/app_test.dart
@@ -1,3 +1,6 @@
+@Timeout(Duration(minutes: 30))
+library;
+
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc_app_template/main.dart' as app;
 import 'package:flutter_test/flutter_test.dart';
@@ -14,15 +17,11 @@ void main() {
     app.main([]);
     await tester.pumpAndSettle();
 
-    expect(find.text('Launches'), findsOneWidget);
-    expect(find.text('Emails'), findsOneWidget);
-    expect(find.text('Settings'), findsOneWidget);
-
     expect(find.textContaining('Mission'), findsAtLeast(1));
 
     await tester.fling(find.byType(ListView), const Offset(0, -300), 1000);
     await tester.pumpAndSettle();
 
     expect(find.textContaining('Mission'), findsAtLeast(1));
-  });
+  }, timeout: const Timeout(Duration(minutes: 20)));
 }
