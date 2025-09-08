4bca4cc46224f8ad0edbeabb34b98ac83812a0a4
Oleksii Shtanko <shtankopro@gmail.com>
2025-09-08 22:02:06 +0100

    Add cores repository impl

--- lib/repository/cores_repository.dart
+++ lib/repository/cores_repository.dart
@@ -1,3 +1,6 @@
+import 'package:flutter_bloc_app_template/data/network/api_result.dart';
+import 'package:flutter_bloc_app_template/data/network/data_source/cores_network_data_source.dart';
+import 'package:flutter_bloc_app_template/models/core/core_ext.dart';
 import 'package:flutter_bloc_app_template/models/core/core_resource.dart';
 
 abstract class CoresRepository {
@@ -8,4 +11,41 @@ abstract class CoresRepository {
   });
 
   Future<CoreResource> getCore(String coreSerial);
+}
+
+class CoresRepositoryImpl implements CoresRepository {
+  CoresRepositoryImpl(this._coresDataSource);
+
+  final CoresDataSource _coresDataSource;
+
+  @override
+  Future<List<CoreResource>> getCores(
+      {bool? hasId = true, int? limit, int? offset}) async {
+    final list = await _coresDataSource.getCores(
+      hasId: hasId,
+      limit: limit,
+      offset: offset,
+    );
+
+    return ApiResultWhen(list).when(
+      success: (data) => data.map((e) => e.toResource()).toList(),
+      error: (message) => throw Exception(message),
+      loading: () {
+        throw Exception('Loading');
+      },
+    );
+  }
+
+  @override
+  Future<CoreResource> getCore(String coreSerial) async {
+    final result = await _coresDataSource.getCore(coreSerial);
+
+    return ApiResultWhen(result).when(
+      success: (data) => data.toResource(),
+      error: (message) => throw Exception(message),
+      loading: () {
+        throw Exception('Loading');
+      },
+    );
+  }
 }
