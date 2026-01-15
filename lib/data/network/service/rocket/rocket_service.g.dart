7d132ddcab2d48f6fe51dea02bdf541e68ce5dea
Oleksii Shtanko <shtankopro@gmail.com>
2026-01-15 11:26:12 +0000

    Upgrade dependecies

--- lib/data/network/service/rocket/rocket_service.g.dart
+++ lib/data/network/service/rocket/rocket_service.g.dart
@@ -56,7 +56,7 @@ class _RocketService implements RocketService {
           )
           .toList();
     } on Object catch (e, s) {
-      errorLogger?.logError(e, s, _options, _result);
+      errorLogger?.logError(e, s, _options, response: _result);
       rethrow;
     }
     return _value;
@@ -83,7 +83,7 @@ class _RocketService implements RocketService {
     try {
       _value = NetworkRocketModel.fromJson(_result.data!);
     } on Object catch (e, s) {
-      errorLogger?.logError(e, s, _options, _result);
+      errorLogger?.logError(e, s, _options, response: _result);
       rethrow;
     }
     return _value;
